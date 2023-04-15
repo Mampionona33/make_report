f81327260687801a955f732978e411d4791d2af1
Mampionona33 <ramamps33@gmail.com>
2023-04-15 22:05:47 +0300

    finish calculate DTPT

--- src/routes/updloadOutage.js
+++ src/routes/updloadOutage.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import csvParser from "csv-parser";
 import multer from "multer";
 import checkUploadFile from "./middleware/checkUploadFile";
+import format from "format-duration";
 
 export const router = Router();
 
@@ -12,19 +13,39 @@ const uploadOutage = multer({ dest: "upload/" });
 router.post(
   "/uploadOutage",
   uploadOutage.single("csvFile"),
+
   checkUploadFile,
+
   function (req, res) {
     const results = [];
     fs.createReadStream(req.file.path, "utf-8")
       .on("error", (er) => console.log(er))
       .pipe(csvParser())
       .on("data", (row) => {
-        console.log(`data : ${row}`);
+        JSON.stringify(row);
         results.push(row);
+
+        // Delete upload csv from upload folder
+        fs.unlink(req.file.path, (err) => console.log(err));
       })
       .on("end", () => {
-        const jsonData = JSON.stringify(results);
-        res.send(jsonData);
+        const sumOssDuration = results.reduce(
+          (acc, cur) => acc + Number(cur["OOS Duration"]),
+          0
+        );
+
+        const dtptNoCumul = sumOssDuration / 7 / 526;
+
+        // const data = {
+        //   sumOssDuration,
+        // };
+        res.render("dailyReport", {
+          sumOssDuration: format(dtptNoCumul * 60 * 60 * 24 - 1),
+        });
+        // res.json({
+        //   status: 200,
+        //   data,
+        // });
       });
   }
 );
