519ea39048d7d206bbbc0cfcc47a0cc6b968b7ed
mampionona33 <ramamps33@gmail.com>
2023-04-18 12:36:25 +0300

    create branche ssr/prod

---
+++ public/js/fullCalendarUtils.js
@@ -0,0 +1,22 @@
+function getEvents() {
+  // votre code
+}
+
+function handleEventClick() {
+  // votre code
+}
+
+function addMoreButton(dayHeader) {
+  // votre code
+}
+
+function handleMoreLinkClick(arg) {
+  // votre code
+}
+
+module.exports = {
+  getEvents,
+  handleEventClick,
+  addMoreButton,
+  handleMoreLinkClick
+};
