d46ffe6d22742459fb927c4accfdc0e06f7eb7f2
mampionona33 <ramamps33@gmail.com>
2023-04-18 09:52:15 +0300

    finish to setup caledar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const fullCalendarElement = document.getElementById("full-calendar");
   if (fullCalendarElement) {
     const calendar = new FullCalendar.Calendar(fullCalendarElement, {
+      themeSystem: "bootstrap5",
+      height: 650,
       headerToolbar: {
         left: "prev,next today",
         center: "title",
