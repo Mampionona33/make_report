f525f97685406302528c4d92c8a6f22462ff884f
Mampionona33 <ramamps33@gmail.com>
2023-04-19 19:49:24 +0300

    initialise project

--- src/models/outageModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import outageSchemas from "../schema/outageSchemas";
-
-const Outage = mongoose.model("Outage", outageSchemas);
-
-console.log(Outage); // ajouter cette ligne
-
-export default Outage;
