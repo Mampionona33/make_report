e638285d8a6fee23edfb22fb73c2b4ab7c66dec0
mampionona33 <ramamps33@gmail.com>
2023-04-20 11:26:16 +0300

    try to setup calendar cell click

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
 
 const Calendar = () => {
+  const [dateClicked, setDateClicked] = useState(true);
+
+  const handleDateClick = (info: DateClickArg) => {
+    setDateClicked(!dateClicked);
+
+    alert("test");
+
+    if (dateClicked) {
+      info.dayEl.style.backgroundColor = "red";
+    } else {
+      info.dayEl.style.backgroundColor = "rgb(59 130 246 / 0.0)";
+    }
+  };
+
   return (
     <React.Fragment>
       <FullCalendar
@@ -16,6 +30,7 @@ const Calendar = () => {
           center: "title",
           end: "today dayGridYear,dayGridMonth,timeGridWeek",
         }}
+        dateClick={handleDateClick}
       />
     </React.Fragment>
   );
