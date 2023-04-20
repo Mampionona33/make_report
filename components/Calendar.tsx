cf70a7a4a82d9a6c380834b27e1ef49321307c96
mampionona33 <ramamps33@gmail.com>
2023-04-20 10:31:56 +0300

    finish setup calendar

---
+++ components/Calendar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+const Calendar = () => {
+  return (
+    <React.Fragment>
+      <FullCalendar
+        contentHeight={"80vh"}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={"dayGridMonth"}
+        headerToolbar={{
+          left: "prev next",
+          center: "title",
+          end: "today dayGridYear,dayGridMonth,timeGridWeek",
+        }}
+      />
+    </React.Fragment>
+  );
+};
+
+export default Calendar;
