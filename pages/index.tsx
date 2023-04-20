bec78d8545083aa20b2e54f11a3b8255fae219f8
mampionona33 <ramamps33@gmail.com>
2023-04-20 10:33:48 +0300

    center calendar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <main className="container mx-auto box-border h-screen">
+    <main className="container mx-auto box-border h-screen flex items-center">
       <div className="flex flex-1 items-center justify-center">
         <div className="w-full h-80vh p-5">
           <Calendar />
