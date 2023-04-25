1dbca6fa29913625f2895f861c0e097c9f2c1584
mampionona33 <ramamps33@gmail.com>
2023-04-25 14:00:22 +0300

    finish right side bar

--- components/SideBarRight/SideBarRight.tsx
+++ components/SideBarRight/SideBarRight.tsx
@@ -1,18 +1,15 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 
 const SideBarRight = () => {
+  const [file, setFile] = useState<File | undefined>();
+  const handleInputChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    setFile(ev.target.files?.[0]);
+  };
   return (
-    <>
-      <div className="sideBarContainer">
-        <input type="file" value="import CSV" accept=".csv" />
-        <input
-          type="button"
-          className="button"
-          value={"Submit"}
-          onClick={(ev) => console.log("test")}
-        />
-      </div>
-    </>
+    <form className="sideBarContainer" action="">
+      <input type="file" accept=".csv" onChange={handleInputChange} />
+      <input type="submit" className="button" value={"Submit"} />
+    </form>
   );
 };
 
