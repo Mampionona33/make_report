979f88546ab4e566c6266bbe6e7ed358fd661f13
mampionona33 <ramamps33@gmail.com>
2023-04-26 12:47:37 +0300

    install date-picker

--- components/SideBarRight/SideBarRight.tsx
+++ components/SideBarRight/SideBarRight.tsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import CSVReader from "react-csv-reader";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import "react-datepicker/dist/react-datepicker.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 const SideBarRight = () => {
   const [data, setData] = useState<any>([]);
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
 
   const handleOnFileLoad = (csvData: any[]) => {
     setData(csvData);
@@ -10,21 +16,35 @@ const SideBarRight = () => {
 
   return (
     <div className="sideBarContainer">
-      <CSVReader
-        onFileLoaded={handleOnFileLoad}
-        parserOptions={{ header: true }}
-        inputId="csv-input"
-        inputStyle={{ display: "none" }}
-      />
-      <label htmlFor="csv-input" className="button">
-        Upload CSV
-      </label>
-      <input
-        type="submit"
-        className="button"
-        value="Submit"
-        onClick={() => console.log(data)}
-      />
+      <div className="sideBarContainer__form">
+        <div className="sideBarContainer__form__items">
+          <label htmlFor="date-picker">Select date</label>
+          <DatePicker
+            inline
+            id="date-picker"
+            selected={startDate}
+            dateFormat={"dd/mm/yyyy"}
+            onChange={(date: Date) => setStartDate(date)}
+          />
+        </div>
+        <div className="sideBarContainer__form__items">
+          <CSVReader
+            onFileLoaded={handleOnFileLoad}
+            parserOptions={{ header: true }}
+            inputId="csv-input"
+            inputStyle={{ display: "none" }}
+          />
+          <label htmlFor="csv-input" className="button">
+            Upload CSV
+          </label>
+        </div>
+        <input
+          type="submit"
+          className="button"
+          value="Submit"
+          onClick={() => console.log(startDate, data)}
+        />
+      </div>
     </div>
   );
 };
